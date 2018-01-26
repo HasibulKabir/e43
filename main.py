9f85f8b7e104ab00728d1d590c731be4d8a2ac5c
Koyu Berteon <julianericain@outlook.com>
2018-01-26 18:23:30 +0100

    Guess I fixed that thing with /conv

--- main.py
+++ main.py
@@ -58,60 +58,6 @@ def handle(msg):
             os.system("ffmpeg -ss 0 -t 60 -y -i " + filename + " -strict -2 -ac 1 -map 0:a -codec:a opus -b:a 128k -vn output.ogg")
         sendVoice(chat_id, "output.ogg")
     if content_type == "text":
-        if "😂" in msg['text']:
-            count = len(msg['text'].split("😂")) - 1
-            f = open("counters/joy.txt", "r")
-            s = f.read()
-            f.close()
-            if s == "":
-                s = "0"
-            sum = int(count) + int(s)
-            f = open("counters/joy.txt", "w")
-            f.write(str(sum))
-            f.close()
-            if sum % rnumber == 0:
-                bot.sendMessage(chat_id, "😂 level is now: " + str(sum))
-        if "bro" in msg['text']:
-            count = len(msg['text'].split("bro")) - 1
-            f = open("counters/bro.txt", "r")
-            s = f.read()
-            f.close()
-            if s == "":
-                s = "0"
-            sum = int(count) + int(s)
-            f = open("counters/bro.txt", "w")
-            f.write(str(sum))
-            f.close()
-            if sum % rnumber == 0:
-                bot.sendMessage(chat_id, "bro level is now: " + str(sum))
-        if "Hi" in msg['text']:
-            count = len(msg['text'].split("Hi")) - 1
-            f = open("counters/hi.txt", "r")
-            s = f.read()
-            f.close()
-            if s == "":
-                s = "0"
-            sum = int(count) + int(s)
-            f = open("counters/hi.txt", "w")
-            f.write(str(sum))
-            f.close()
-            if sum % rnumber == 0:
-                bot.sendMessage(chat_id, "Hi level is now: " + str(sum))
-        if "lol" in msg['text']:
-            count = len(msg['text'].split("lol")) - 1
-            f = open("counters/lol.txt", "r")
-            s = f.read()
-            f.close()
-            if s == "":
-                s = "0"
-            sum = int(count) + int(s)
-            f = open("counters/lol.txt", "w")
-            f.write(str(sum))
-            f.close()
-            if sum % rnumber == 0:
-                bot.sendMessage(chat_id, "lol level is now: " + str(sum))
-        if msg['text'].startswith("/start"):
-            bot.sendMessage(chat_id,"Hello, please send me the name of the song or an URL from Soundcloud, YouTube and many more I have to convert :)")
         if msg['text'].startswith("/conv http://") or msg['text'].startswith("/conv https://") and not chat_type == "channel":
             if " &" in msg['text']:
                 bot.sendMessage(chat_id, "Uh-oh, something bad happened. Note that Telegram limits bot uploads to 50MB. Otherwise contact @Sommerlichter for further assistance.")
@@ -133,47 +79,102 @@ def handle(msg):
                     bot.sendMessage(chat_id,"Here you go!")
                 except:
                     bot.sendMessage(chat_id, "Uh-oh, something bad happened. Note that Telegram limits bot uploads to 50MB. Otherwise contact @Sommerlichter for further assistance.")
-        if chat_type == "private" and msg["text"].startswith("http"):
-            if " &" in msg['text']:
-                bot.sendMessage(chat_id, "Uh-oh, something bad happened. Note that Telegram limits bot uploads to 50MB. Otherwise contact @Sommerlichter for further assistance.")
-            else:
-                bot.sendMessage(chat_id, "Please wait...I'm converting the URL to an MP3 file")
-                try:
-                    filename = os.popen("node --no-warnings download-url.js " + msg['text']).read().rstrip()
-                    bot.sendMessage(chat_id, "Sending the file...")
-                    sendAudio(chat_id, filename)
-                    audio = MP3(filename)
-                    length = audio.info.length * 0.33
-                    l2 = length + 60
-                    if audio.info.length > l2:
-                       os.system("ffmpeg -ss " + str(length) + " -t 60 -y -i \"" + filename + "\" -strict -2 -ac 1 -map 0:a -codec:a opus -b:a 128k -vbr off output.ogg")
-                    else:
-                        os.system("ffmpeg -ss 0 -t 60 -y -i \"" + filename + "\" -strict -2 -ac 1 -map 0:a -codec:a opus -b:a 128k -vbr off output.ogg")
-                    sendVoice(chat_id, "output.ogg")
-                    bot.sendMessage(chat_id,"Here you go!")
-                except:
+        else:
+            if "😂" in msg['text']:
+                count = len(msg['text'].split("😂")) - 1
+                f = open("counters/joy.txt", "r")
+                s = f.read()
+                f.close()
+                if s == "":
+                    s = "0"
+                sum = int(count) + int(s)
+                f = open("counters/joy.txt", "w")
+                f.write(str(sum))
+                f.close()
+                if sum % rnumber == 0:
+                    bot.sendMessage(chat_id, "😂 level is now: " + str(sum))
+            if "bro" in msg['text']:
+                count = len(msg['text'].split("bro")) - 1
+                f = open("counters/bro.txt", "r")
+                s = f.read()
+                f.close()
+                if s == "":
+                    s = "0"
+                sum = int(count) + int(s)
+                f = open("counters/bro.txt", "w")
+                f.write(str(sum))
+                f.close()
+                if sum % rnumber == 0:
+                    bot.sendMessage(chat_id, "bro level is now: " + str(sum))
+            if "Hi" in msg['text']:
+                count = len(msg['text'].split("Hi")) - 1
+                f = open("counters/hi.txt", "r")
+                s = f.read()
+                f.close()
+                if s == "":
+                    s = "0"
+                sum = int(count) + int(s)
+                f = open("counters/hi.txt", "w")
+                f.write(str(sum))
+                f.close()
+                if sum % rnumber == 0:
+                    bot.sendMessage(chat_id, "Hi level is now: " + str(sum))
+            if "lol" in msg['text']:
+                count = len(msg['text'].split("lol")) - 1
+                f = open("counters/lol.txt", "r")
+                s = f.read()
+                f.close()
+                if s == "":
+                    s = "0"
+                sum = int(count) + int(s)
+                f = open("counters/lol.txt", "w")
+                f.write(str(sum))
+                f.close()
+                if sum % rnumber == 0:
+                    bot.sendMessage(chat_id, "lol level is now: " + str(sum))
+            if msg['text'].startswith("/start"):
+                bot.sendMessage(chat_id,"Hello, please send me the name of the song or an URL from Soundcloud, YouTube and many more I have to convert :)")
+            if chat_type == "private" and msg["text"].startswith("http"):
+                if " &" in msg['text']:
                     bot.sendMessage(chat_id, "Uh-oh, something bad happened. Note that Telegram limits bot uploads to 50MB. Otherwise contact @Sommerlichter for further assistance.")
-        if chat_type == "private" and not msg['text'].startswith("/start") and not msg['text'].startswith("http"):
-            if " &" in msg['text']:
-                bot.sendMessage(chat_id, "Uh-oh, something bad happened. Note that Telegram limits bot uploads to 50MB. Otherwise contact @Sommerlichter for further assistance.")
-            else:
-                try:
-                    bot.sendMessage(chat_id, "Please wait...I'm converting the song to an MP3 file")
-                    metadata = os.popen("node --no-warnings download.js " + msg['text']).read().rstrip()
-                    filename = os.popen("node --no-warnings download-url.js " + metadata).read().rstrip()
-                    bot.sendMessage(chat_id, "Sending the file...")
-                    sendAudio(chat_id, filename)
-                    audio = MP3(filename)
-                    length = audio.info.length * 0.33
-                    l2 = (audio.info.length * 0.33) + 60
-                    if audio.info.length > l2:
+                else:
+                    bot.sendMessage(chat_id, "Please wait...I'm converting the URL to an MP3 file")
+                    try:
+                        filename = os.popen("node --no-warnings download-url.js " + msg['text']).read().rstrip()
+                        bot.sendMessage(chat_id, "Sending the file...")
+                        sendAudio(chat_id, filename)
+                        audio = MP3(filename)
+                        length = audio.info.length * 0.33
+                        l2 = length + 60
+                        if audio.info.length > l2:
                         os.system("ffmpeg -ss " + str(length) + " -t 60 -y -i \"" + filename + "\" -strict -2 -ac 1 -map 0:a -codec:a opus -b:a 128k -vbr off output.ogg")
-                    else:
-                        os.system("ffmpeg -ss 0 -t 60 -y -i \"" + filename + "\" -strict -2 -ac 1 -map 0:a -codec:a opus -b:a 128k -vbr off output.ogg")
-                    sendVoice(chat_id, "output.ogg")
-                    bot.sendMessage(chat_id,"Here you go!\nConsider a small donation at https://koyu.space/support if you like this bot :)")
-                except:
-                    bot.sendMessage(chat_id, "I cannot find the song you're looking for. Go find yourself a link and enter it here, so I know where to start from.")
+                        else:
+                            os.system("ffmpeg -ss 0 -t 60 -y -i \"" + filename + "\" -strict -2 -ac 1 -map 0:a -codec:a opus -b:a 128k -vbr off output.ogg")
+                        sendVoice(chat_id, "output.ogg")
+                        bot.sendMessage(chat_id,"Here you go!")
+                    except:
+                        bot.sendMessage(chat_id, "Uh-oh, something bad happened. Note that Telegram limits bot uploads to 50MB. Otherwise contact @Sommerlichter for further assistance.")
+            if chat_type == "private" and not msg['text'].startswith("/start") and not msg['text'].startswith("http") and not msg['text'].startswith("/conv"):
+                if " &" in msg['text']:
+                    bot.sendMessage(chat_id, "Uh-oh, something bad happened. Note that Telegram limits bot uploads to 50MB. Otherwise contact @Sommerlichter for further assistance.")
+                else:
+                    try:
+                        bot.sendMessage(chat_id, "Please wait...I'm converting the song to an MP3 file")
+                        metadata = os.popen("node --no-warnings download.js " + msg['text']).read().rstrip()
+                        filename = os.popen("node --no-warnings download-url.js " + metadata).read().rstrip()
+                        bot.sendMessage(chat_id, "Sending the file...")
+                        sendAudio(chat_id, filename)
+                        audio = MP3(filename)
+                        length = audio.info.length * 0.33
+                        l2 = (audio.info.length * 0.33) + 60
+                        if audio.info.length > l2:
+                            os.system("ffmpeg -ss " + str(length) + " -t 60 -y -i \"" + filename + "\" -strict -2 -ac 1 -map 0:a -codec:a opus -b:a 128k -vbr off output.ogg")
+                        else:
+                            os.system("ffmpeg -ss 0 -t 60 -y -i \"" + filename + "\" -strict -2 -ac 1 -map 0:a -codec:a opus -b:a 128k -vbr off output.ogg")
+                        sendVoice(chat_id, "output.ogg")
+                        bot.sendMessage(chat_id,"Here you go!\nConsider a small donation at https://koyu.space/support if you like this bot :)")
+                    except:
+                        bot.sendMessage(chat_id, "I cannot find the song you're looking for. Go find yourself a link and enter it here, so I know where to start from.")
 
 def sendAudio(chat_id,file_name):
     url = "https://api.telegram.org/bot%s/sendAudio"%(TOKEN)
